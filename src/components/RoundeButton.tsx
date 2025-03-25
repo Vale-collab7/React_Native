8740500cf862962a17ced87b203beb8025a2a23f
Vale-collab7 <vv783650@gmail.com>
2025-03-25 17:40:29 -0500

    Add files via upload

---
+++ src/components/RoundeButton.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { TouchableOpacity, Text, StyleSheet}  from 'react-native'
+
+
+interface Props {
+    text:string;
+    onPress: () =>void,
+}
+export const RoundeButton = ({text, onPress}: Props) => {
+  return (
+      <TouchableOpacity
+      style={styles.RoundeButton}
+      onPress={() => onPress()}>
+      
+    <Text style={styles.textButton}>{text}</Text>
+      </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+    RoundeButton: {
+        width: '100%',
+        height: 50, 
+        backgroundColor: 'orange', 
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 15,
+    },
+    textButton: {
+        color: 'white', 
+    },
+
+});
